de941f9c660cdadc839083c48f7b095ccc0c5fbb
Christoph Müller <christoph.mueller@upljft.com>
2019-11-28 21:32:01 +0100

    Add work references

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -1,11 +1,1 @@
 import { createBrotliCompress } from "zlib";
-import Highway from '@dogstudio/highway';
-import Fade from './transition';
-
-console.log(`I was loaded at ${Date(Date.now()).toString()}`);
-
-const H = new Highway.Core({
-  transitions: {
-    default: Fade
-  }
-});
