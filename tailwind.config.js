eb8efb5ed069bb90c40bc38d41e03987833f297f
Christoph Müller <christoph.mueller@upljft.com>
2020-05-12 14:24:58 +0200

    Change color and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
       colors: {
         black: '#2a2927',
         white: '#f8f8f8'
+      },
+      fontFamily: {
+        display: ['Gotu'],
+        body: ['Inter'],
       }
     },
   },
